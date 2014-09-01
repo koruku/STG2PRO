18e002dc931741c395eeafc513d7f852670d1836
koruku <hiroshi@senku.jp>
2014-09-01 19:07:09 +0900

    Update s2p_apply.php

--- classes/s2p_apply.php
+++ classes/s2p_apply.php
@@ -6,9 +6,9 @@ $modx = new DocumentParser;
 $modx->db->connect();
 $modx->getSettings();
 
-include_once( MODX_BASE_PATH . "assets/modules/s2pmodule/classes/s2pmodule.class.php" );
-include_once( MODX_BASE_PATH . "assets/modules/s2pmodule/classes/s2p_backend.class.php" );
-
+include_once( MODX_BASE_PATH . "assets/modules/s2pmodule/classes/s2pmodule.class.php" );   //～読み込み
+include_once( MODX_BASE_PATH . "assets/modules/s2pmodule/classes/s2p_backend.class.php" );  //～読み込み
+//～以下データ読み込み
 $s2p = new s2pModule( $modx );
 $s2pb = new s2pModuleBackend( $s2p, $modx );
 
